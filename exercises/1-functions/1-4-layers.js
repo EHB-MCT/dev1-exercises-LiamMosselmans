793d5b924f05cb2cb8a48ad77bbaf3416625c675
Liam Mosselmans <liam.mosselmans@student.ehb.be>
2022-10-05 15:39:25 +0200

    1-4 recreated example using layering

---
+++ exercises/1-functions/1-4-layers.js
@@ -0,0 +1,19 @@
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+context.fillStyle = 'lightblue';
+context.strokeStyle = 'black';
+
+drawLayers();
+
+function drawLayers() {
+    context.beginPath();
+    context.rect(50,50,300,300);
+    context.rect(50,50,250,250);
+    context.rect(100,50,200,200);
+    context.rect(150,100,150,150);
+    context.rect(150,150,100,100);
+    context.fill();
+    context.stroke();
+}
