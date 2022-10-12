7f36b6bc0f705e5e01a0e856f698cc2ba7868d3e
Liam Mosselmans <liam.mosselmans@student.ehb.be>
2022-10-12 15:39:24 +0200

    2-3 Pattern [Started]

---
+++ exercises/2-variables/2-3-pattern.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+let smallRectangleWidth = window.innerWidth / 3;
+let smallRectangleHeight = window.innerHeight / 5;
+let bigRectangleWidth = smallRectangleWidth * 3;
+let bigRectangleHeight = smallRectangleHeight * 2;
+
+createPattern(smallRectangleWidth,smallRectangleHeight,bigRectangleWidth,bigRectangleHeight);
+
+function createPattern(smallWidth, smallHeight, bigWidth, bigHeight)
+{
+    context.beginPath();
+    context.fillRect(0,0,smallWidth,smallHeight);
+    context.fillRect(smallWidth * 2, 0, smallWidth, smallHeight);
+    context.fillStyle = "orange";
+    context.fillRect(0, smallHeight, bigWidth, bigHeight);
+}
