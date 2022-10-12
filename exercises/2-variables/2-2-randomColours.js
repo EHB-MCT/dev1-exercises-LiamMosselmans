6cb5dc921b3fea1b4f2bdb7530744579a5d9c1b8
Liam Mosselmans <liam.mosselmans@student.ehb.be>
2022-10-12 15:38:33 +0200

    2-2 Random Colours [Improved random color generation code]

--- exercises/2-variables/2-2-randomColours.js
+++ exercises/2-variables/2-2-randomColours.js
@@ -8,27 +8,20 @@ let context = canvas.getContext('2d');
 let margin = 0;
 let squareSize = 800;
 
-let colorRandomOne;
-let colorRandomTwo;
-let colorRandomThree;
-let colorSum;
-
 for(let i = 0; i < 10; i++)
 {
-    coloredSquares(margin, squareSize, colorSum);
+    coloredSquares(margin, squareSize);
 }
 
-
-function coloredSquares(marginSquares, sizeSquares, color)
+function coloredSquares(marginSquares, sizeSquares)
 {
-    colorSum = "#" + colorRandomOne + colorRandomOne + colorRandomTwo + colorRandomTwo + colorRandomThree + colorRandomThree;
-    context.fillStyle = color;
-    colorRandomOne = Math.floor(Math.random() * 10);
-    colorRandomTwo = Math.floor(Math.random() * 10);
-    colorRandomThree = Math.floor(Math.random() * 10);
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    let color = "rgb(" + red + "," + green + "," + blue + ")";
 
     context.beginPath();
-    context.fillStyle = "#" + colorRandomOne + colorRandomOne + colorRandomTwo + colorRandomTwo + colorRandomThree + colorRandomThree;
+    context.fillStyle = color;
     context.fillRect(marginSquares, marginSquares, sizeSquares, sizeSquares);
     margin += 50;
     squareSize -= 100;
