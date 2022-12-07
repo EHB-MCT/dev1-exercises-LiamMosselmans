8717d68a7fa1170c398c5e4acb630e80339d4614
Liam Mosselmans <liam.mosselmans@student.ehb.be>
2022-12-07 13:38:12 +0100

    8-3 glitchyTriangles [PROGRESSED]

--- exercises/8-objects/8-3-glitchyTriangles.js
+++ exercises/8-objects/8-3-glitchyTriangles.js
@@ -5,3 +5,37 @@ import * as Utils from "../../scripts/utils.js";
 
 let width = context.canvas.width;
 let height = context.canvas.height;
+
+let triangles = [];
+
+context.fillStyle = 'red';
+context.beginPath();
+context.moveTo(50,50);
+context.lineTo(300,300);
+context.lineTo(550,50);
+context.lineTo(10,10);
+context.fill();
+
+
+function setup() {
+    context.fillStyle = 'darkcyan';
+    context.fillRect(0, 0, width, height);
+
+    for(let i = 0; i < 30; i++) {
+        let triangle = {
+            strokeLength: 100,
+            x: Utils.randomNumber(100, width - 100),
+            y: height + 200,
+            hue: Utils.randomNumber(0, 360)
+        }
+        triangles[i] = triangle;
+    }
+}
+
+function update() {
+
+}
+
+function drawTriangle() {
+
+}
